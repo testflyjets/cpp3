8dd63ca7f5f2ec7a844729872406048134469796
Chris McCann <testflyjets@gmail.com>
2015-01-28 23:53:41 -0800

    Completes Coin implementation

--- proj1/Coin.cpp
+++ proj1/Coin.cpp
@@ -10,19 +10,17 @@
 Project1::Coin::Coin(CoinType denomination)
     : denomination(denomination)
 {
-    // TODO: Implement
+
 }
 
 Project1::Coin::CoinType
 Project1::Coin::getDenomination() const
 {
-    // TODO: Implement
-    return Coin::COINTYPE_WOODEN_NICKEL;
+    return denomination;
 }
 
 unsigned
 Project1::Coin::getValueCents() const
 {
-    // TODO: Implement
-    return 0;
+    return denomination;
 }
